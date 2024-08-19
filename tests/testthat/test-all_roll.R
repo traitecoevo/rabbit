1bdb362783c346e9fdd52cb8a90997fa65aa2547
Daniel Falster <daniel.falster@unsw.edu.au>
2024-08-20 04:52:58 +1000

    optimise  method for sliding windows

--- tests/testthat/test-all_roll.R
+++ tests/testthat/test-all_roll.R
@@ -12,16 +12,21 @@ test_that("whole thing doesn't error out and matches older version", {
   }
   
   new <- moving_window_calcs(df[1:50,])
+  
   expect_type(new, "list")
   
   # Comparee new and old caluclations, 
   # excluding the date column that we're handling different on purpose
   # Try several startign points
+  window_size <- 50
+  
   for(i in c(1, 101, 151)) {
     ii <- seq(i, i+49)
     new <- moving_window_calcs(df[ii,])[50, -1]
+    new2 <- moving_window_calcs_df(df[ii,])[50, -1]
     old <- doAccloop(as.matrix(df[ii,c(6,3:5)]))[,-1]
   
     expect_equal(new, old, tolerance=0.0001) 
+    expect_equal(new2, old, tolerance=0.0001) 
   }
 })
