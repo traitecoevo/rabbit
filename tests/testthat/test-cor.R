e2cb2bc613f048c2e1d8c67d35e13bb5e2b2e985
Will Cornwell <w.cornwell@unsw.edu.au>
2024-08-19 10:06:45 +1000

    cleaning up

--- tests/testthat/test-cor.R
+++ tests/testthat/test-cor.R
@@ -1,11 +1,11 @@
 test_that("correlation works", {
-  x<-rnorm(50)
-  y<-rnorm(50)
-  test_value<-rolling_correlation(x,y,50)[50]
-  expect_equal(cor(x,y),test_value)
+  x <- rnorm(50)
+  y <- rnorm(50)
+  test_value <- rolling_correlation(x, y, 50)[50]
+  expect_equal(cor(x, y), test_value)
   
-  x2<-rnorm(100)
-  y2<-rnorm(100)
-  test_value2<-rolling_correlation(x2,y2,50)[100]
-  expect_equal(cor(x2[51:100],y2[51:100]),test_value2)
+  x2 <- rnorm(100)
+  y2 <- rnorm(100)
+  test_value2 <- rolling_correlation(x2, y2, 50)[100]
+  expect_equal(cor(x2[51:100], y2[51:100]), test_value2)
 })
