7a1f2ebc2c5dd8c1366d67a082d54dd375d87017
Will Cornwell <w.cornwell@unsw.edu.au>
2024-08-26 14:55:22 +1000

    predicting

--- R/classify.R
+++ R/classify.R
@@ -1,18 +1,3 @@
-
-classify_and_plot<-function(window_minutes){
-library(arrow)
-library(rabbit)
-aa<-read_parquet("tests/testthat/raw_Pic2Jan_50000.parquet")
-
-dat<-moving_window_calcs_2(aa)
-load(file = "MSOM_8by7.rda")
-dd<-as.matrix(dat[,-1])
-
-ssom.pred <- predict(MSOM, newdata = dd, whatmap = 1)
-
-dat$behavior<-ssom.pred$predictions$activity
-print(table(dat$behavior))
-
 # Function to calculate mode
 calculate_mode <- function(x) {
   ux <- unique(x)
@@ -20,22 +5,48 @@ calculate_mode <- function(x) {
 }
 
 # Function to calculate mode behavior for each window of N minutes
-mode_behavior_by_window <- function(data, time_col, behavior_col, window_minutes) {
+plot_mode_behavior_by_window <- function(data,
+                                    time_col,
+                                    behavior_col,
+                                    window_minutes) {
   data %>%
-    mutate(time = ymd_hms(!!sym(time_col)),  # Ensure the time column is in POSIXct format
+    mutate(time = ymd_hms(!!sym(time_col)),
+           # Ensure the time column is in POSIXct format
            window = floor_date(time, unit = paste0(window_minutes, " mins"))) %>%
     group_by(window) %>%
     summarise(mode_behavior = calculate_mode(!!sym(behavior_col))) %>%
-    filter(!is.na(mode_behavior)) ->mode_sum
-    mode_sum %>%
+    filter(!is.na(mode_behavior)) -> mode_sum
+  mode_sum %>%
     ggplot(aes(x = window, y = mode_behavior, color = mode_behavior)) +
     geom_point(size = 3) +
-    labs(x = "Time", y = "Mode Behavior", title = paste0("Mode Behavior Every ", window_minutes, " Minutes")) +
-    theme_minimal()->plotting
-    print(plotting)
+    labs(
+      x = "Time",
+      y = "Mode Behavior",
+      title = paste0("Mode Behavior Every ", window_minutes, " Minutes")
+    ) +
+    theme_minimal() -> plotting
+  print(plotting)
   return(mode_sum)
 }
 
-mode_sum<-mode_behavior_by_window(dat, "time", "behavior", window_minutes)
-return(mode_sum)
+
+classify_behaviors <- function(
+                               acc_dat=read_parquet("tests/testthat/raw_Pic2Jan_50000.parquet"),
+                               MSOM_path="tests/testthat/MSOM_8by7.rda",
+                               quiet=FALSE
+                               ) {
+  
+  dat <- moving_window_calcs_2(acc_dat) #this might move out of the function
+  dd <- as.matrix(dat[, -1])
+  load(file = MSOM_path)
+  
+  ssom.pred <- predict(MSOM, newdata = dd, whatmap = 1) # this is slow part
+  
+  dat$behavior <- ssom.pred$predictions$activity
+  if(quiet) print(table(dat$behavior))
+  
+  return(dat)
 }
+
+
+
