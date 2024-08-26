d0ec360f0ab28128ea28c6328b6afbf228d0be21
Will Cornwell <w.cornwell@unsw.edu.au>
2024-08-26 12:13:58 +1000

    plot_funx

--- R/classify.R
+++ R/classify.R
@@ -1,5 +1,5 @@
 
-classify_and_plot<-function(){
+classify_and_plot<-function(window_minutes){
 library(arrow)
 library(rabbit)
 aa<-read_parquet("tests/testthat/raw_Pic2Jan_50000.parquet")
@@ -9,13 +9,9 @@ load(file = "MSOM_8by7.rda")
 dd<-as.matrix(dat[,-1])
 
 ssom.pred <- predict(MSOM, newdata = dd, whatmap = 1)
-tibble(time_date=dat$time,ssom.pred$predictions)
-table(ssom.pred$predictions$activity)
-dat$behavior<-ssom.pred$predictions$activity
-table(dat$behavior)
-summary(dat$time)
-ggplot(dat,aes(x=time,y=behavior,col=behavior))+geom_point(size=1,alpha=0.1)
 
+dat$behavior<-ssom.pred$predictions$activity
+print(table(dat$behavior))
 
 # Function to calculate mode
 calculate_mode <- function(x) {
@@ -30,11 +26,16 @@ mode_behavior_by_window <- function(data, time_col, behavior_col, window_minutes
            window = floor_date(time, unit = paste0(window_minutes, " mins"))) %>%
     group_by(window) %>%
     summarise(mode_behavior = calculate_mode(!!sym(behavior_col))) %>%
+    filter(!is.na(mode_behavior)) ->mode_sum
+    mode_sum %>%
     ggplot(aes(x = window, y = mode_behavior, color = mode_behavior)) +
     geom_point(size = 3) +
     labs(x = "Time", y = "Mode Behavior", title = paste0("Mode Behavior Every ", window_minutes, " Minutes")) +
-    theme_minimal()
+    theme_minimal()->plotting
+    print(plotting)
+  return(mode_sum)
 }
 
-mode_behavior_by_window(dat, "time", "behavior", 0.1)
+mode_sum<-mode_behavior_by_window(dat, "time", "behavior", window_minutes)
+return(mode_sum)
 }
