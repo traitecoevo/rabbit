092678d6abfa49627e9b9d6546cfe0c7b623fbf7
Daniel Falster <daniel.falster@unsw.edu.au>
2026-02-27 21:36:08 +1100

    Refactor cropping functions: replace crop_times with crop_ends, add crop_ends_diurnal and crop_ends_nocturnal, and update documentation accordingly

--- R/crop_data.R
+++ R/crop_data.R
@@ -1,38 +1,43 @@
 
-
-#' This function Crops the start and end of each file to a specified time. Use crop_diurnal for diurnal data and crop_nocturnal for nocturnal data.
-#'
+#' Crop data to specified start and end times, to remove records at the beginning and end of each file.
+#' This is useful for removing records collected before the device was attached or after it was removed. crop_diurnal defaults to cropping dataframe to the first instance of 01:00:00 and the last instance of 01:00:00. crop_nocturnal defaults to cropping dataframe to the first instance of 12:00:00 and the last instance of 12:00:00. crop_ends is a more general function that allows users to specify any start and end times. crop_diurnal
 #' @param df A dataframe containing the data.
 #' @param start_time The desired start time
 #' @param end_time The desired end time
-#' @rdname crop_times
+#' @rdname crop_ends
 #' @export
-crop_times <- function(df, start_time = "00:00:00", end_time   = "24:00:00") {
-  
-  if(end_time <= start_time) {
-    stop("in crop_times: end_time must be after start_time")
+crop_ends <- function(df, start_time = "00:00:00", end_time   = "24:00:00") {
+
+  # first rows with time > start_time and last rows with time < end_time are retained. 
+  time <- format(df$time, "%H:%M:%S")
+
+  start_i <- which(!is.na(time) & time >= start_time)
+  if(length(start_i) == 0) {
+    start_i <- nrow(df)
+  } else {
+    start_i <- min(start_i)
+  }
+
+  # if there are no times after end_time, then end_i is set to nrow(df) to retain all rows after start_i. Otherwise, end_i is set to the last row with time < end_time.
+  end_i <- which(!is.na(time) &  time <= end_time)
+  if(length(end_i) == 0) {
+    end_i <- nrow(df)
+  } else {
+    end_i <- max(end_i)
   }
 
   df |>
-    dplyr::filter(
-      dplyr::between(
-        format(df$time, "%H:%M:%S"), start_time, end_time)
-    )
+    dplyr::slice(start_i:end_i)
 }
 
-#' @rdname crop_times
+#' @rdname crop_ends
 #' @export
-crop_diurnal <- function(df, start_time = "06:00:00", end_time = "18:00:00") {
-  crop_times(df, start_time = start_time, end_time = end_time)
+crop_ends_diurnal <- function(df, start_time = "01:00:00", end_time = "01:00:00") {
+  crop_ends(df, start_time = start_time, end_time = end_time)
 }
 
-#' @rdname crop_times
+#' @rdname crop_ends
 #' @export
-crop_nocturnal <- function(df, start_time = "18:00:00", end_time = "06:00:00") {
-  df |>
-    dplyr::filter(
-      !dplyr::between(
-        format(df$time, "%H:%M:%S"), end_time, start_time
-      )
-    )
+crop_ends_nocturnal <- function(df, start_time = "12:00:00", end_time = "12:00:00") {
+  crop_ends(df, start_time = start_time, end_time = end_time)
 }
--- tests/testthat/test-crop_data.R
+++ tests/testthat/test-crop_data.R
@@ -1,41 +1,36 @@
-test_that("crop_times trims to matching time window", {
+test_that("crop_ends keeps rows between first and last matched times", {
   df <- make_time_df(n = 48)
 
-  # any times
   expect_no_error(
-    out <- crop_times(df, start_time = "12:00:00", end_time = "13:00:00")
+    out <- crop_ends(df, start_time = "12:00:00", end_time = "13:00:00")
   )
 
-  time <- out$time |> format("%H:%M:%S")
-  expect_true(all(time >= "12:00:00"))
-  expect_true(all(time <= "13:00:00"))
+  time <- format(df$time, "%H:%M:%S")
+  expected_idx <- min(which(time >= "12:00:00")):max(which(time <= "13:00:00"))
 
-  # dirunal
-  expect_no_error(
-    out <- crop_diurnal(df)
-  )
-  time <- out$time |> format("%H:%M:%S")
-  expect_true(all(time >= "06:00:00"))
-  expect_true(all(time <= "18:00:00"))
-  
-  expect_no_error(
-    out <- crop_diurnal(df, start_time = "11:00", end_time = "13:00")
-  )
-  time <- out$time |> format("%H:%M:%S")
-  expect_true(all(time >= "11:00:00"))
-  expect_true(all(time <= "13:00:00"))
-  
-  ## nocturnal
-  expect_no_error(
-    out <- crop_nocturnal(df)
-  )
-  time <- out$time |> format("%H:%M:%S")
-  expect_true(all((time >= "18:00:00") | (time <= "06:00:00")))
+  expect_equal(nrow(out), length(expected_idx))
+  expect_equal(out$time, df$time[expected_idx])
+  expect_equal(format(out$time[1], "%H:%M:%S"), "12:00:00")
+  expect_equal(format(out$time[nrow(out)], "%H:%M:%S"), "13:00:00")
+})
 
-  expect_no_error(
-    out <- crop_nocturnal(df, start_time = "22:00:00", end_time = "04:00:00")
-  )
-  time <- out$time |> format("%H:%M:%S")
-  expect_true(all((time >= "22:00:00") | (time <= "04:00:00")))
+test_that("crop_ends uses fallback indices when no times match", {
+  df <- make_time_df(n = 48)
 
+  out <- crop_ends(df, start_time = "25:00:00", end_time = "26:00:00")
+
+  expect_equal(nrow(out), 1)
+  expect_equal(out$time, df$time[48])
+})
+
+test_that("crop_ends wrappers call crop_ends with expected defaults", {
+  df <- make_time_df(n = 48)
+
+  out_diurnal <- crop_ends_diurnal(df)
+  expected_diurnal <- crop_ends(df, start_time = "01:00:00", end_time = "01:00:00")
+  expect_equal(out_diurnal, expected_diurnal)
+
+  out_nocturnal <- crop_ends_nocturnal(df)
+  expected_nocturnal <- crop_ends(df, start_time = "12:00:00", end_time = "12:00:00")
+  expect_equal(out_nocturnal, expected_nocturnal)
 })
